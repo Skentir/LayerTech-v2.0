da2c712fec4dabf5c601f531d8647183a8e30d92
danjohnvelasco <54620209+danjohnvelasco@users.noreply.github.com>
2020-12-08 00:52:53 +0800

    added vuetify datatable

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'import/no-extraneous-dependencies' : ["error", {"devDependencies": true}],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'linebreak-style': 0,
   },
   overrides: [
     {
