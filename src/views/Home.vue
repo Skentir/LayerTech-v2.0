7e786649a42690ef54376db623ba470c8c75d153
danjohnvelasco <54620209+danjohnvelasco@users.noreply.github.com>
2020-12-08 20:40:32 +0800

    generalized table component (added props)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <Table/>
+    <Table
+      :tableData="this.componentData[0].data"
+      :tableCols="this.componentData[0].headers"
+      :tableTitle="tableTitle"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Table from '@/components/Table.vue';
+import dessertData from '@/models/desserts.json';
 
 export default {
   name: 'Home',
   components: {
     Table,
+  },
+  data() {
+    return {
+      tableTitle: 'Nutrition',
+      componentData: dessertData,
+    };
+  },
+  mounted() {
+    console.log(this.componentData[0].data);
   },
 };
 </script>
