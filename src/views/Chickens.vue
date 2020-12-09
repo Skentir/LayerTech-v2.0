f9002d72adf83f3399b19fba9bfb7b6dec334c87
JohannGon118 <johann_gonzales@dlsu.edu.ph>
2020-12-09 10:13:00 +0800

    added chickens dummy data

---
+++ src/views/Chickens.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="chickens">
+    <PageTemplate :title="title" :description="description" :addLabel="addLabel"/>
+    <Table
+      :tableData="this.componentData[0].data"
+      :tableCols="this.componentData[0].headers"
+      :tableTitle="tableTitle"
+    />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Table from '@/components/Table.vue';
+import PageTemplate from '@/components/PageTemplate.vue';
+import chickenData from '@/models/chickens.json';
+
+export default {
+  name: 'Employees',
+  components: {
+    Table,
+    PageTemplate,
+  },
+  data() {
+    return {
+      title: 'Chickens',
+      description: ' This page is solely about chickens This page is solely about chickens This page is solely about chickens This page is solely about chickens This page is solely about chickens ',
+      addLabel: 'Add Chicken',
+      tableTitle: 'Chickens',
+      componentData: chickenData,
+    };
+  },
+  methods: {
+    someFunc() {
+      console.log('this is someFunc()');
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
