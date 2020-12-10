55c0395826b25c496359a8c6c7a84f3e753c396f
JohannGon118 <johann_gonzales@dlsu.edu.ph>
2020-12-10 20:27:46 +0800

    updated FormChicken to display MM/DD/YYYY date format

--- src/components/FormChicken.vue
+++ src/components/FormChicken.vue
@@ -36,11 +36,46 @@
             <v-col cols="6">
               <v-text-field label="Vaccination Schedule" required/>
             </v-col>
-            <v-col cols="12">
-              <v-text-field label="Date Recieved" required/>
+            <v-col cols="6">
+              <v-menu
+                ref="menu"
+                v-model="menu"
+                :close-on-content-click="false"
+                :return-value.sync="date"
+                transition="scale-transition"
+                offset-y
+                min-width="290px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="dateFormatted"
+                    label="Date Recieved"
+                    hint="MM/DD/YYYY"
+                    persistent-hint
+                    prepend-icon="mdi-calendar"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="date"
+                  no-title
+                  scrollable
+                >
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    text
+                    color="primary"
+                    @click="$refs.menu.save(date)"
+                  >
+                    OK
+                  </v-btn>
+                </v-date-picker>
+              </v-menu>
             </v-col>
-            <v-col cols="12">
-              <v-text-field label="Chicken Type" required/>
+            <v-col cols="6">
+              <v-text-field label="Person in-Charge" required/>
             </v-col>
             <v-col cols="6">
               <v-text-field label="Section Assigned" required/>
@@ -74,11 +109,37 @@
 
 <script>
 export default {
-  name: 'FormOperations',
-  data() {
-    return {
-      dialog: false,
-    };
+// taken from veutifyjs
+  data: (vm) => ({
+    date: new Date().toISOString().substr(0, 10),
+    dateFormatted: vm.formatDate(new Date().toISOString().substr(0, 10)),
+    menu: false,
+  }),
+  computed: {
+    computedDateFormatted() {
+      return this.formatDate(this.date);
+    },
+  },
+
+  watch: {
+    date() {
+      this.dateFormatted = this.formatDate(this.date);
+    },
+  },
+
+  methods: {
+    formatDate(date) {
+      if (!date) return null;
+
+      const [year, month, day] = date.split('-');
+      return `${month}/${day}/${year}`;
+    },
+    parseDate(date) {
+      if (!date) return null;
+
+      const [month, day, year] = date.split('/');
+      return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
+    },
   },
 };
 </script>
