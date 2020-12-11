b595455d4de8f4c25c939a5955a081ada57f5b29
JohannGon118 <johann_gonzales@dlsu.edu.ph>
2020-12-11 20:40:17 +0800

    removed unused portions

--- src/views/Employees.vue
+++ src/views/Employees.vue
@@ -1,24 +1,17 @@
 <template>
   <div class="employees">
     <PageTemplate :pageInfo="pageInfo"/>
-    <Table
-      :tableData="this.componentData[0].data"
-      :tableCols="this.componentData[0].headers"
-      :tableTitle="tableTitle"
-    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Table from '@/components/Table.vue';
 import PageTemplate from '@/components/PageTemplate.vue';
 import employeeData from '@/models/employees.json';
 
 export default {
   name: 'Employees',
   components: {
-    Table,
     PageTemplate,
   },
   data() {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <Table
-      :tableData="this.componentData[0].data"
-      :tableCols="this.componentData[0].headers"
-      :tableTitle="tableTitle"
-    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Table from '@/components/Table.vue';
+
 import dessertData from '@/models/desserts.json';
 
 export default {
   name: 'Home',
   components: {
-    Table,
   },
   data() {
     return {
