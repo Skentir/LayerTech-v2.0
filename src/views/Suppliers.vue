cd3dd622b9ef967a777e0c78200c03f0028df531
danjohnvelasco <54620209+danjohnvelasco@users.noreply.github.com>
2020-12-11 17:01:24 +0800

    removed unused lines

--- src/views/Suppliers.vue
+++ src/views/Suppliers.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="supplier">
     <h1>This is a suppliers page</h1>
-    <Table/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Table from '@/components/Table.vue';
-
 export default {
   name: 'Suppliers',
   components: {
-    Table,
   },
   data() {
     return {
