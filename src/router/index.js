058b72760cd2fe3c75712978956710e8f0e51b44
Axel Alba <albaaxel@yahoo.com>
2020-12-12 16:02:37 +0800

    removed about page in app.vue

--- src/router/index.js
+++ src/router/index.js
@@ -19,14 +19,6 @@ const routes = [
     component: Login,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-  {
     path: '/employees',
     name: 'Employees',
     component: Employees,
