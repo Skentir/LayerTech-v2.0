6cd25a4f8a38ef0938031829275a4ec7b33fd27b
danjohnvelasco <54620209+danjohnvelasco@users.noreply.github.com>
2020-12-12 17:51:07 +0800

    setup unit test, passed first test

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,5 @@
+/* eslint-disable max-len */
+/* eslint-disable max-len */
 <template>
   <div id="login">
     <div class="loginWidget">
@@ -8,15 +10,24 @@
             <h3 class="splash-text"
             >An inventory and stock management web application for your farm</h3>
             <br>
-            <v-text-field class="tField" v-model="username" background-color="rgba(255,255,255,1)"
-            outlined="true" placeholder="Username"></v-text-field>
+            <v-text-field
+              id="username_tfield"
+              class="tField"
+              v-model="username"
+              background-color="rgba(255,255,255,1)"
+              outlined
+              placeholder="Username"
+            >
+            </v-text-field>
             <v-text-field class="tField" v-model="password" background-color="rgba(255,255,255,1)"
-            outlined="true" placeholder="Password"></v-text-field>
+            outlined placeholder="Password"></v-text-field>
             <v-btn type="submit" id="login-btn" color="rgba(4,35,178,1)" class="lButton"
             elevation="2">Log In</v-btn>
             <br>
             <br>
-            <h3 id="error-msg" v-if="hasError">The username and password you entered did not match our records. Please double-check and try again.</h3> <!-- Error message container -->
+            <h3 id="error-msg" v-if="hasError">
+              {{errorMsg}}
+            </h3> <!-- Error message container -->
         </v-form>
     </div>
   </div>
@@ -34,11 +45,17 @@ export default {
       password: null,
       hasError: false, // for whether status message will show or not
       submitted: false,
+      errorMsg: 'The username and password you entered did not match our records. Please double-check and try again.',
     };
   },
   methods: {
     handleSubmit() {
-      this.submitted = true;
+      /*
+        Handles empty username and password
+      */
+      if (this.username === '' || this.username == null || this.password === '' || this.password == null) {
+        this.hasError = true;
+      }
     },
   },
 };
--- tests/unit/login.spec.js
+++ tests/unit/login.spec.js
@@ -1,11 +1,28 @@
-import { mount } from '@vue/test-utils';
+// Imports
+import Vuetify from 'vuetify';
 import Login from '@/views/Login.vue';
 
+// Utilities
+import { createLocalVue, mount } from '@vue/test-utils';
+
 describe('Login.vue', () => {
-  it('reveals an error message when login failed', async () => {
-    const wrapper = mount(Login);
-    await wrapper.find('[data-username]').setValue('');
-    await wrapper.find('[data-password]').setValue('');
+  const localVue = createLocalVue();
+  let vuetify;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+  });
+
+  const mountFunction = (options) => mount(Login, {
+    localVue,
+    vuetify,
+    ...options,
+  });
+
+  it('reveals an error message when login failed due to empty username or password', async () => {
+    const wrapper = mountFunction();
+    await wrapper.find('#username_tfield').setValue('');
+    // await wrapper.find('[data-password]').setValue('');
     await wrapper.find('form').trigger('submit.prevent');
     expect(wrapper.find('#error-msg').text())
       .toBe('The username and password you entered did not match our records. Please double-check and try again.');
