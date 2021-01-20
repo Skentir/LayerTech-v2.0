1f4ab34e6a1d1a2e0a14b733551aad015f73dfb9
danjohnvelasco <54620209+danjohnvelasco@users.noreply.github.com>
2021-01-20 19:25:08 +0800

    added mongodb atlas url

--- api/server.js
+++ api/server.js
@@ -1,9 +1,30 @@
 const express = require('express');
-const { envPort, sessionKey } = require('./config');
+const { envPort, sessionKey, dbURL } = require('./config');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
 
 // create express app
 const app = express();
 const port = envPort || 3000;
+
+// setup mongoose
+const options = { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false 
+};
+
+mongoose.connect(dbURL, options);
+
+// Sessions
+app.use(session({
+  secret: sessionKey,
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24 * 7 }
+}));
 
 // Setup middlewares
 app.use(express.json()); // support json encoded bodies
