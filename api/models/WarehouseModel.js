b9947fd0176f07148bb8891a284fd5a3fc086c68
unknown <nick.intatano@gmail.com>
2021-01-30 19:20:10 +0800

    Modified Schema to match Story

--- api/models/WarehouseModel.js
+++ api/models/WarehouseModel.js
@@ -1,21 +1,26 @@
 const { Schema, model} = require('mongoose');
 
 const WarehouseSchema = new Schema({
-    //IDs
-    item_id: {type: String, required: true},
-    serial_id: {type: String, required: true},
-    //fields
-    item_name: {type: String, required: true},
+    //attributes present in add item modal
+    serial_id: {type: String, unique: true, required: true},
+    supplier: {type: ObjectId, required: true},
+    product_title: {type: String, required: true},
     product_type: {type: String, required: true},
+    product_code: {type: String, required: true},
+    dosage: {type: String, required: true},
+    received_date: {type: Date, required: true, default: Date.now},
+    expiration_date: {type: Date, required: true, default: Date.now},
     stock_quantity: {type: Number, min: 0, required: true},
-    pulled_out_quantity: {type: Number, min: 0, required: true},
-    liquidated_quantity: {type: Number, min: 0, required: true},
     critical_volume: {type: Number, min: 0, required: true},
     unit: {type: String, required: true},
-    expiration_date: {type: Date, required: true, default: Date.now},
-    received_date: {type: Date, required: true, default: Date.now},
-    dosage: {type: String, required: true},
-    status: {type: String, required: true},
+    packaging: {type: String, required: true},
+    //hidden attributes
+    batch_number: {type: Number, min: 0, required: true},
+    batch_status: {type: String, required: true},
+    product_status: {type: String, required: true},
+    product_status: {type: String, required: true},
+    pulled_out_quantity: {type: Number, min: 0, required: true},
+    liquidated_quantity: {type: Number, min: 0, required: true},
 })
 
 const WarehouseModel = model('warehouse', WarehouseSchema);
