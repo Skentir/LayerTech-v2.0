0db70429c10b9849ce8184a6f2bd51903abe2c6e
unknown <nick.intatano@gmail.com>
2021-02-05 23:19:50 +0800

    Removed console.log()s in warehouse route

--- api/routes/warehouse.js
+++ api/routes/warehouse.js
@@ -8,7 +8,6 @@ router.get('/', async (req, res) => {
     try{
         console.log('Tried');
         const warehouse_items_list = await Warehouse.find();
-        console.log(warehouse_items_list);
         if(!warehouse_items_list) throw new Error('No Warehouse Items')
         
         res.status(200).json(warehouse_items_list);
@@ -42,7 +41,6 @@ router.put('/:id', async (req, res) => {
         const updated = { ...response._doc, ...req.body}
         res.status(200).json(updated);
     }catch(error){
-        console.log(error.message)
         res.status(500).json({ message: error.message })
     }
 })   
