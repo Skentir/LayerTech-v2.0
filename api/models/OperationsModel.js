4217dfc698025686e6a720f1b1520d4f8702e949
unknown <nick.intatano@gmail.com>
2021-02-05 23:18:16 +0800

    Modified Operations schema

--- api/models/OperationsModel.js
+++ api/models/OperationsModel.js
@@ -2,13 +2,24 @@ const { Schema, model} = require('mongoose');
 
 const OperationsSchema = new Schema({
     serial_id: {type: String, required: true},
-    username: {type: String, required: true},
-    quantity: {type: Number, min: 0, required: true},
-    original_quantity: {type: Number, min: 0, required: true},
-    entry: {type: String, required: true, enum: ['IN', 'OUT']},
-    s_eggs_sold: {type: Number, min: 0, required: true},
-    m_eggs_sold: {type: Number, min: 0, required: true},
-    l_eggs_sold: {type: Number, min: 0, required: true},
+    //supplier: {type: Schema.Types.ObjectId, ref: 'suppliers', required: true},
+    batch_number: {type: Number, min: 0, required: true},
+    product_status: {type: String, required: true},
+    supplier: {type: String, required: true},
+    product_title: {type: String, required: true},
+    product_type: {type: String, required: true},
+    product_code: {type: String, required: true},
+    received_date: {type: Date, required: true, default: Date.now},
+    expiration_date: {type: Date, required: true, default: Date.now},
+    unit: {type: String, required: true},
+    packaging: {type: String, required: true},
+    entry: {type: String, required: true, enum: ["IN", "OUT"]},
+    dosage: {type: String, required: false},
+    quantity: {type: Number, required: true, min: 0},
+    original_quantity: {type: Number, required: false, min: 0},
+    eggs_sold_s: {type: Number, required: false, min: 0},
+    eggs_sold_m: {type: Number, required: false, min: 0},
+    eggs_sold_l: {type: Number, required: false, min: 0},
 })
 
 const OperationsItem = model('operations', OperationsSchema);
