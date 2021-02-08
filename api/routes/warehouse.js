c491280c7c087ebf948798bdccb527e1f7a08d84
unknown <nick.intatano@gmail.com>
2021-02-08 10:27:47 +0800

    Added basic notification functionality for Warehouse

--- api/routes/warehouse.js
+++ api/routes/warehouse.js
@@ -56,4 +56,39 @@ router.delete('/:id', async (req, res) => {
     }
 })
 
+router.get('/alerts', async (req, res) => {
+    try {
+        const warehouse_items_list = await Warehouse.find();
+        if(!warehouse_items_list) throw new Error('No Warehouse Items')
+
+        const alertsList = []
+        warehouse_items_list.forEach((item, index) => {
+            var notifType = 0
+            if(item.stock_quantity <= item.critical_volume && item.stock_quantity > 1){
+                notifType=1
+            }
+            const current = Date.now();
+            const expDate = new Date(item.expiration_date);
+            const diffTime = Math.abs(expDate - current);
+            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+            if(diffDays == 1){
+                notifType=3
+            }
+            else if(diffDays <= 7){
+                notifType=2
+            }
+            if(notifType){
+                alertsList.push({
+                    batch_number: item.batch_number,
+                    item_name: item.product_title,
+                    type: notifType,
+                })
+            }
+        })
+        res.status(200).json(alertsList)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
 module.exports = router;
