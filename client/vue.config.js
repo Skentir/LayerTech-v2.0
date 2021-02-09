9b4af8010e8a341fc7b7f5ace13660d5531f6639
danjohnvelasco <54620209+danjohnvelasco@users.noreply.github.com>
2021-02-09 15:44:52 +0800

    deploy to production

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,17 +1,5 @@
-const { envPort } = require('../api/config');
-
 module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
-  configureWebpack: {
-    devServer: {
-      proxy: {
-        '/api': {
-          target: `http://localhost:${envPort}`,
-          changeOrigin: true
-        },
-      }
-    }
-  },
 };
